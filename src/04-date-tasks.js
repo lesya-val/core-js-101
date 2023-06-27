7b91e20aa08cd696d195e043ec7f9b49121705bf
lesya-val <you@example.com>
2023-06-27 19:24:23 +0600

    feat: finish date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -53,8 +53,9 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0);
 }
 
 
@@ -73,8 +74,13 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const time = new Date(endDate.getTime() - startDate.getTime());
+  const hours = time.getUTCHours().toString().padStart(2, '0');
+  const minutes = time.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = time.getUTCSeconds().toString().padStart(2, '0');
+  const milliseconds = time.getUTCMilliseconds().toString().padStart(3, '0');
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 
@@ -94,8 +100,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() % 12;
+  const minutes = date.getUTCMinutes();
+  let angle = Math.abs((30 * hours + 0.5 * minutes) - (6 * minutes));
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return angle * (Math.PI / 180);
 }
 
 
